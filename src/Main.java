2498368e14117a7045f6e376132cca34c0a29060
venuhari82 <venuhari82@gmail.com>
2024-09-09 21:09:59 +0530

    sample

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String str= "test";
+        System.out.println("The String values is: "+str);
+
+        String str1 = new String("test");
+        str1 = "test";
+        System.out.println(str1);
+        if(str == str1)
+        {
+            System.out.println("Both string are equal");
+        }
+        else {System.out.println("not Equal");}
+
+        if (str.equals(str1)){
+            System.out.println("Both values are equal");
+        }
+
+    }
+}
